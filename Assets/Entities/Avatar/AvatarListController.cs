c95ee25801f10239c97e78fd7059dc8a5249567b
Anders Tolborg <anders@tolborg.dk>
2016-09-07 23:58:55 +0200

    first commit

---
+++ Assets/Entities/Avatar/AvatarListController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvatarListController : MonoBehaviour {
+
+	public GameObject avatarListItemPrefab;
+	public Avatar[] avatars;
+
+
+	void Start() {
+
+		TextAsset targetFile = Resources.Load<TextAsset>("Gamedata/avatars");
+		avatars = JsonHelper.FromJson<Avatar>(targetFile.text);
+
+
+		foreach (Avatar avatar in avatars) {
+			GameObject avatarListItem = Instantiate(avatarListItemPrefab) as GameObject;
+			AvatarListItemController avatarListItemController = avatarListItem.GetComponent<AvatarListItemController>();
+
+			avatarListItemController.SetData(avatar);
+			avatarListItem.transform.SetParent(transform);
+		}
+
+
+	}
+
+
+
+
+
+}
---
+++ Assets/Entities/Avatar/AvatarListItemController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+[Serializable]
+public class Avatar {
+	public string avatarName;
+}
+
+
+
+
+
+public class AvatarListItemController : MonoBehaviour {
+
+	public Text avatarName;
+
+
+	public void SetData(Avatar avatar) {
+		avatarName.text = avatar.avatarName;
+	}
+
+
+}
